3c0525c85d02b2ad426c69b586a431412db14dde
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-04-29 01:00:03 +0600

    Done - input word, submit, fetch api, render return data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit.prevent="getDefinitions()">
+    <input type="text" placeholder="Type a word" id="word" v-model="word">
+    <button type="submit">Get definitions</button>
+  </form>
+  <div>    
+    <div v-if="word">
+      <p>Definitions for word <i>{{ word }}</i></p>
+    </div>
+    <p v-if="!validQuery">Error: the word you are searching does not exist.</p>
+    <div v-for="definition in definitionsArray" :key="definition">
+      <div>{{ definition }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const word = ref("");
+    let validQuery = ref(true);
+    const definitionsArray = ref([]);
+
+    async function getDefinitions() {
+      definitionsArray.value.splice(0, definitionsArray.value.length);
+      validQuery.value = true;
+
+      try {
+        const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word.value;
+        const { data } = await axios.get(url);
+
+        data.map(({ meanings }) => {
+          meanings.map(({ definitions }) => {
+            definitions.forEach(({ definition }) => {
+              if (typeof definition === 'string') {
+                definitionsArray.value.push(definition);
+              }
+            });
+          })
+        })
+      } catch (error) {
+        validQuery.value = false;
+      }
+    }
+    
+    return { word, getDefinitions, validQuery, definitionsArray };
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
