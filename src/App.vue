842605d4c87ad4021b258ad06a01b775f818e0ac
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-04-29 01:28:32 +0600

    Done: design with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <form @submit.prevent="getDefinitions()">
-    <input type="text" placeholder="Type a word" id="word" v-model="word">
-    <button type="submit">Get definitions</button>
-  </form>
-  <div>    
-    <div v-if="word">
-      <p>Definitions for word <i>{{ word }}</i></p>
+  <div class="container">
+    <div style="background-color: black; padding: 20px 0px; text-align: center; color: white; text-transform: uppercase;">
+      <h1>Dictionary App</h1>
     </div>
-    <p v-if="!validQuery">Error: the word you are searching does not exist.</p>
-    <div v-for="definition in definitionsArray" :key="definition">
-      <div>{{ definition }}</div>
+    <div style="padding: 10px 0px;">
+      <form @submit.prevent="getDefinitions()">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text me-1" id="inputGroup-sizing-default">Word</span>
+          </div>
+          <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Type a word" id="word" v-model="word">
+        </div>
+        <button class="btn btn-success" type="submit">Get definitions</button>
+      </form>
+    </div>
+    <div>    
+      <div v-if="word">
+        <p>Definitions for word <i>{{ word }}</i></p>
+      </div>
+      <p v-if="!validQuery">Error: the word you are searching does not exist.</p>
+      <div v-for="(definition, index) in definitionsArray" :key="definition">
+        <!-- <div>{{ definition }}</div> -->
+        <div class="card bg-dark" style="margin-top: 5px;">
+          <div class="card-body">
+            <p class="card-text text-warning">{{ index + 1 }}</p>
+            <h5 class="card-title text-white">{{ definition }}</h5>            
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 createApp(App).mount('#app')
